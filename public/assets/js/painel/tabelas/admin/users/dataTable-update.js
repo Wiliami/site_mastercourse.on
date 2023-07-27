c723368137ac8213c6dc328d2b1280676374a45a
wiliami <wiliaup97@gmail.com>
2023-07-27 17:58:26 -0400

    update code

---
+++ public/assets/js/painel/tabelas/admin/users/dataTable-update.js
@@ -0,0 +1,28 @@
+  firebase.auth().onAuthStateChanged(userAuthenticated => {
+    if (userAuthenticated) {
+        findUsers(userAuthenticated);
+    }
+  });
+  
+  function findUsers() {
+    firebase.firestore()
+    .collection('users')
+    .get()
+    .then(snapshot => {
+        const users = snapshot.docs.map(doc => doc.data());
+        addUsersToScreen(users);
+    });
+  }
+
+
+  function addUsersToScreen(users) {
+
+    users.forEach(user => {
+        const name = document.getElementById('username');
+        name.innerHTML = user.name;
+
+        const email = document.getElementById('user-email');
+        email.innerHTML = user.email;
+    });
+  }
+  
