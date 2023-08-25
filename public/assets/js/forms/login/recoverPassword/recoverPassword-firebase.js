3286121008827e7ea5f126f8391f580292657812
wiliami <wiliaup97@gmail.com>
2023-08-25 13:37:01 -0400

    delete file

--- public/assets/js/forms/login/recoverPassword/recoverPassword-firebase.js
+++
@@ -1,9 +0,0 @@
-function recoverPassword() {
-    firebase.auth().sendPasswordResetEmail(
-    form.email().value
-    ).then(() => {
-        alert('Email enviado com sucesso!');
-    }).catch(error => {
-        alert(getErrorMessage(error));
-    });
-}
