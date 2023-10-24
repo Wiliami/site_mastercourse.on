94931abb24a9b00e271a206d32b6924d207cb2e5
wiliami <wiliaup97@gmail.com>
2023-10-23 23:34:28 -0400

    updates code

--- routes/register.js
+++ routes/register.js
@@ -9,14 +9,20 @@ router.get('/', (req, res) => {
 
 router.post('/', async(req, res) => {
     const { username, email, password, confirmPassword } = req.body;
+ 
+    if(username && email && password && confirmPassword) {
+        if(password.length < 6) {
+            console.log('A senha deve ter pelo menos 6 caracteres.');
+            res.status(400).send('A senha deve ter pelo menos 6 caracteres.');
+            return;
+        }
 
-    
-    if(!password || password.length < 6) {
-        return res.status(400).send('A senha deve ter pelo menos 6 caracteres.');
-    }
+        if(password !== confirmPassword) {
+            console.log('As senhas não coincidem.');
+            res.status(400).send('As senhas não coincidem.');
+            return;
+        }
 
-    
-    if(username, email, password, confirmPassword) {
         try {
             const createUser = await admin.auth().createUser({
                 displayName: username,
@@ -30,24 +36,17 @@ router.post('/', async(req, res) => {
                 email,
                 update_date: new Date(),
             });
-    
+
             console.log('Usuário cadastrado com sucesso: ', createUser.uid);
             res.status(201).send('Usuário cadastrado com sucesso!');
         } catch (error) {
-            // if(error.code === 'auth/email-already-exists') {
-            //     console.log('Este e-mail já está cadastrado por outro usuário!');
-            //     res.status(400).send('Este e-mail já está cadastrado por outro usuário!');
-            // } else {
-            // }
             console.log('Erro ao criar usuário: ', error);
             res.status(500).send('Erro ao criar usuário');
         }
     } else {
         console.log('Preencha todos os dados');
+        return res.status(400).send('Preencha todos os campos.');
     }
-
-
-   
 
 });
 
--- routes/verificarEmail.js
+++ routes/verificarEmail.js
@@ -6,28 +6,22 @@ router.post('/', async (req, res) => {
     const { email } = req.body;
     console.log('Email recebido:', email || 'E-mail não informado.');
 
-  
-    if(!email) {
-        console.error('Preencha todos os campos.');
-        return res.status(400).send('Preencha todos os campos.');
-    } else {
-        try {
-            const userRef = db.collection('users');
-            const snapshot = await userRef.where('email', '==', email).get();
-            
-            if(!snapshot.empty) {
-                console.log('Este e-mail já está cadastrado.');
-                res.status(400).send('E-mail já está em uso.');
-                return;
-            } else {
-                console.log('E-mail disponível para cadastro.');
-                res.status(200).send('E-mail disponível para cadastro.');
-            }
-        } catch (error) {
-            console.error('Erro ao verificar o e-mail: ', error);
-            res.status(500).send('Erro ao verificar o e-mail.');
+
+    try {
+        const userRef = db.collection('users');
+        const snapshot = await userRef.where('email', '==', email).get();
+        
+        if(!snapshot.empty) {
+            console.log('Este e-mail já está cadastrado.');
+            res.status(400).send('Este e-mail já está cadastrado.');
+            return;
         }
-    }    
+
+    } catch (error) {
+        console.error('Erro ao verificar o e-mail: ', error);
+        res.status(500).send('Erro ao verificar o e-mail.');
+    }
+
 
 });
 
