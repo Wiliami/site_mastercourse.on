99179f1738dcb0f90c5d5951bfc1095c9ba30035
wiliami <wiliaup97@gmail.com>
2023-12-27 06:52:03 -0400

    update code

---
+++ index.spec.js
@@ -0,0 +1,6 @@
+describe('Sample Test', () => {
+    it('Should test that true === true', () => {
+        const result = true;
+        expect(result).toBe(true);
+    })
+})
--- public/assets/js/my-code.test.js
+++
@@ -1,5 +0,0 @@
-const { sum } = require('./my-code')
-
-it('sums 2 numbers', () => {
-    expect(sum(1, 2)).toBe(4)
-})
