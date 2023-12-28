6dd9079042c3954ebfbf54cde980c18b387d6a0b
wiliami <wiliaup97@gmail.com>
2023-12-28 17:30:56 -0400

    add test jest

---
+++ math.js
@@ -0,0 +1,32 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiplication (a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+const shoppingList = [
+    "fraldas",
+    "kleenex",
+    "sacos de lixo",
+    "papel toalha",
+    "leite",
+];
+
+module.exports = {
+    sum,
+    subtract,
+    multiplication,
+    divide,
+    shoppingList
+}
