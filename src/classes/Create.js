177ab0fb8203cf0ff505ca9bbb14b857c748a59a
wiliami <wiliaup97@gmail.com>
2024-01-30 17:41:03 -0400

    update code

--- src/classes/Create.js
+++ src/classes/Create.js
@@ -1,30 +1,14 @@
-import admin from 'firebase-admin'
+import admin from '../../config/firebaseConfig';
+
+
 
 class Create {
-    
     constructor() {
-        admin.initializeApp({
-            credential: admin.credential.cert('serviceAccountKey.json'),
-        });
+
     }
 
-    async createUser(email, password) {
-        try {
-            const userRecord = await admin.auth().createUser({
-                email: email,
-                password: password
-            });
-            console.log('Usuário cadastrado com sucesso:', userRecord.uid);
-            return userRecord.uid;
-        } catch (error) {
-            console.error('Erro ao criar usuário:', error);
-            throw error;
-        }
-    }
+
+
 }
 
-export default Create;
 
-const createUser = new Create();
-createUser.createUser('teste@gmail.com', '123456');
-console.log(createUser);
---
+++ src/classes/CreateUser.js
@@ -0,0 +1,30 @@
+import admin from 'firebase-admin'
+
+class CreateUser {
+    
+    constructor() {
+        admin.initializeApp({
+            credential: admin.credential.cert('../../serviceAccountKey.json'),
+        });
+    }
+
+    async createUser(email, password) {
+        try {
+            const userRecord = await admin.auth().createUser({
+                email: email,
+                password: password
+            });
+            console.log('Usuário cadastrado com sucesso:', userRecord.uid);
+            return userRecord.uid;
+        } catch (error) {
+            console.error('Erro ao criar usuário:', error);
+            throw error;
+        }
+    }
+}
+
+export default CreateUser;
+
+const userRecord = new CreateUser();
+userRecord.createUser('admin.teste@gmail.com', '123456');
+console.log(userRecord);
