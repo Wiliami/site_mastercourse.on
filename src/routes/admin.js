5dc4d9ad2d8d807ca2f7c7af452d203fbb31ff7f
wiliami <wiliaup97@gmail.com>
2024-02-27 08:09:09 -0400

    fixing routes application

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,6 +5,5 @@ router.get('/users/create', (req, res) => res.render('admin/users/create'));
 router.get('/users/list', (req, res) => res.render('admin/users/list'));
 router.get('/users/update', (req, res) => res.render('admin/users/update'));
 router.get('/users/delete', (req, res) => res.render('admin/users/delete'));
-router.get('/dashboard1', (req, res) => res.render('dashboard'));
 
-export default router;
+export default router;  
