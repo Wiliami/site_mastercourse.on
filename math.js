6a66c8e7f692d0897f2c49efdab1b35a326cba25
wiliami <wiliaup97@gmail.com>
2024-04-25 18:44:03 -0400

    add instance class

--- math.js
+++ math.js
@@ -6,4 +6,13 @@ export default class Math {
    static substract(a, b) {
     return a - b;
    }
+
+   static divider(a, b) {
+    return a / b;
+   }
 };
+
+
+console.log('Resposta da soma:', Math.sum(2, 2));
+console.log('Resposta da subtração:', Math.substract(1, 5));
+console.log('Resposta da divisão:', Math.divider(5, 5));
