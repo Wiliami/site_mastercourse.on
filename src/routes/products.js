08ed1dfba9ca117583cde97b19bff44b5b3c8c5e
wiliami <wiliaup97@gmail.com>
2024-05-23 02:02:01 -0400

    add feature

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,27 @@
 import { Router } from 'express';
 const router = Router();
+const { admin, db } = require('../firebaseConfig'); // esse arquivo nã existe
 
-router.get('/create', (req, res) => {
-    res.render('area-membro/products/create');
+router.get('/create', (req, res) => res.render('area-membro/products/create'));
+
+router.post('/create', async (req, res) => {
+    try {
+    
+        const { product, price, quantity } = req.body;
+
+        const docRef = db.collection('products').doc();
+        await docRef.set({
+            product,
+            price,
+            quantity
+        }); 
+
+        res.status(200).send({ message: 'Produto criado com sucesso', id: docRef.id });
+
+    } catch (error) {
+        console.error('Erro ao criar produto:', error);
+        res.status(500).send({ message: 'Erro ao criar produto', error: error.message });
+    }
 });
 
 router.get('/list', (req, res) => {
