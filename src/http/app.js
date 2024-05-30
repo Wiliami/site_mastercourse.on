2720afd3bbcf7658c86c4ac61b21f7c839a7f5ef
wiliami <wiliaup97@gmail.com>
2024-05-30 01:44:49 -0400

    add config fixing path server

--- src/http/app.js
+++ src/http/app.js
@@ -16,7 +16,7 @@ class App {
         const __dirname = path.dirname(__filename);
 
         this.app.use('/assets', express.static(path.join(__dirname, '../../public/assets')));
-        this.app.set('views', path.join(__dirname, 'views'));
+        this.app.set('views', path.join(__dirname, '../views'));
         this.app.engine('.hbs', engine({extname: '.hbs'}));
         this.app.set('view engine', '.hbs');
         this.app.use(express.urlencoded({extended:false}));
