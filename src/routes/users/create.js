2e69d2d680b099def5fae512bf8128d692ab10b4
wiliami <wiliaup97@gmail.com>
2024-07-26 01:25:49 -0400

    feat: new feature

---
+++ src/routes/users/create.js
@@ -0,0 +1,12 @@
+import pool from '../../config/database.js'
+
+const createUser = async (name, email) => {
+    try {
+        const res = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email])
+        console.log('Usuário criado:', res.rows[0])
+    } catch (error) {
+        console.error('Erro ao criar usuário:', error)
+    }
+}
+
+createUser('Bob', 'bob@gmail.com')
