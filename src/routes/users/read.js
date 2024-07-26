6eb61cc633564de547842443a71fcb572d774e0f
wiliami <wiliaup97@gmail.com>
2024-07-26 01:26:31 -0400

    feat: new feature

---
+++ src/routes/users/read.js
@@ -0,0 +1,12 @@
+import pool from '../../config/database.js'
+
+const getUsers = async () => {
+  try {
+    const res = await pool.query('SELECT * FROM users')
+    console.log(res.rows)
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error)
+  }
+}
+
+getUsers()
