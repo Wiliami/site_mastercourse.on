531feacd9c978b9b6c2c560a8abbcc0e5d0d36b0
wiliami <wiliaup97@gmail.com>
2024-10-25 17:33:41 -0400

    feat: new feature

--- src/routes/verificarEmail.js
+++ src/routes/verificarEmail.js
@@ -1,26 +1,21 @@
 import { Router } from 'express';
 const router = Router();
 
+
+//validar email de usuário
+
 router.post('/', async (req, res) => {
   const { email } = req.body;
-  console.log('Email recebido:', email || 'E-mail não informado.');
- 
+  // console.log('Email recebido:', email || 'E-mail não informado.');
+  
   try {
-    const userRecord = await admin.auth().getUserByEmail(email);
-    console.log('Este e-mail já está cadastrado.');
-    // res.status(400).send('Este e-mail já está cadastrado.');
-    res.status(200).json({ isValid: false });
-
-  } catch (error) {
-    if(error.code === 'auth/user-not-found') {
-      console.log('Este e-mail não está cadastrado.');
-      // res.status(200).send('Tudo certo');
-      res.status(200).json({ isValid: true });
+    if(email) {
+      console.log('Email:', email)
     } else {
-      console.error('Erro ao verificar o e-mail');
-      // res.status(500).send('Erro ao verificar o e-mail');
-      res.status(500).json({ isValid: false });
+      console.error('Não existe email')
     }
+  } catch (error) {
+    
   }
 });
 
